a6053b390282413293beb5dd4378ca28fdc44f8c
fsLeg <fsleg@t-rg.ws>
2026-03-20 21:38:51 +0300

    helper script: leave only x86_64 arch

--- update-version.sh
+++ update-version.sh
@@ -21,29 +21,9 @@ if [[ ! -d "$PRGNAM" ]]; then
   exit 1
 fi
 
-if [[ -z "$ARCH" ]]; then
-  case "$(uname -m)" in
-    i?86) ARCH="i586" ;;
-    arm*) ARCH="arm" ;;
-       *) ARCH="$(uname -m)" ;;
-  esac
-fi
-
-if [[ "$ARCH" == i?86 ]]; then
-  DEBARCH="i386"
-  PLATFORM="ia32"
-  BITS="32"
-elif [[ "$ARCH" == "arm" ]]; then
-  DEBARCH="armhf"
-  PLATFORM="arm"
-  BITS="32"
-elif [[ "$ARCH" == "x86_64" ]]; then
+if [[ "${ARCH:=$(uname -m)}" == "x86_64" ]]; then
   DEBARCH="amd64"
   PLATFORM="x64"
-  BITS="64"
-elif [[ "$ARCH" == "aarch64" ]]; then
-  DEBARCH="arm64"
-  PLATFORM="arm64"
   BITS="64"
 fi
 
@@ -54,36 +34,21 @@ sed -i "s|${VERSION/\./\\.}|$NEWVER|g" "$PRGNAM.SlackBuild"
 unset -v VERSION
 source "$PRGNAM.SlackBuild"
 
-for URL in $([[ "$DOWNLOAD" != "UNSUPPORTED" ]] && echo "$DOWNLOAD" || echo "") \
-           $([[ -n "$DOWNLOAD_x86" && "$DOWNLOAD_x86" != "UNSUPPORTED" ]] && echo "$DOWNLOAD_x86" || echo "")
-do
+for URL in $DOWNLOAD; do
   if [[ ! "$URL" =~ ^git\+ && ! -f "$(basename "$URL")" ]]; then
     wget --tries=inf --retry-on-http-error=503 "$URL" || true
   fi
 done
 
-if [[ "$DOWNLOAD" != "UNSUPPORTED" ]]; then
-  for ITEM in $DOWNLOAD; do
-    if [[ "$ITEM" =~ ^git\+ ]]; then
-      CHECKSUMS+="- "
-    else
-      CHECKSUMS+="$(sha256sum "$(basename $ITEM)" | cut -d' ' -f1) "
-    fi
-  done
-  CHECKSUMS="$(echo "${CHECKSUMS% }" | tr ' ' '\n' | awk '{if(NR>1) printf " \\\\\\\n%11s%s", "", $0; else printf "%s", $0}')"
-  perl -0777 -pi -e 's|SHA256SUM="[-0-9a-f\s\\]*"|SHA256SUM="'"$CHECKSUMS"'"|' "$PRGNAM.SlackBuild"
-fi
-if [[ -n "$DOWNLOAD_x86" && "$DOWNLOAD_x86" != "UNSUPPORTED" ]]; then
-  for ITEM in $DOWNLOAD_x86; do
-    if [[ "$ITEM" =~ ^git\+ ]]; then
-      CHECKSUMS32+="- "
-    else
-      CHECKSUMS32+="$(sha256sum "$TARBALL" | cut -d' ' -f1) "
-    fi
-  done
-  CHECKSUMS32="$(echo "${CHECKSUMS32% }" | tr ' ' '\n' | awk '{if(NR>1) printf " \\\\\\\n%15s%s", "", $0; else printf "%s", $0}')"
-  perl -0777 -pi -e 's|SHA256SUM_x86="[0-9a-f\s\\]*"|SHA256SUM_x86="'"$CHECKSUMS32"'"|' "$PRGNAM.SlackBuild"
-fi
+for ITEM in $DOWNLOAD; do
+  if [[ "$ITEM" =~ ^git\+ ]]; then
+    CHECKSUMS+="- "
+  else
+    CHECKSUMS+="$(sha256sum "$(basename $ITEM)" | cut -d' ' -f1) "
+  fi
+done
+CHECKSUMS="$(echo "${CHECKSUMS% }" | tr ' ' '\n' | awk '{if(NR>1) printf " \\\\\\\n%11s%s", "", $0; else printf "%s", $0}')"
+perl -0777 -pi -e 's|SHA256SUM="[-0-9a-f\s\\]*"|SHA256SUM="'"$CHECKSUMS"'"|' "$PRGNAM.SlackBuild"
 
 if [[ -z "$NOBUILD" ]]; then
   time fakeroot "$CWD/sb" "$PRGNAM.SlackBuild"
